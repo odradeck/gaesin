6d8ee5e6b37cf5251bb88490445277988dad5f92
odradeck <odradeck@gmail.com>
2012-12-28 08:38:05 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gaesin::Application.config.secret_token = '1563b4188e9de90020db9a1b976efffb1dd8ce7cd3adbf6760742445a61e7a46c6ac3d77123135580135a2c369d81c582538b3aeda86b28da580912e4c66bd56'
